93b147f274b34371989c2d8a76beb20576b374f0
so2311 <145649675+so2311@users.noreply.github.com>
2023-09-21 10:21:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Internettechnologie 2</title>
+  </head>
+  <body>
+    <h1>Herzlich Willkommen im WS 2024</h1>
+    
+  </body>
+</html>
