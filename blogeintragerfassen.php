2afe4a9608794d50889c8d21d836eec5516248c5
so2311 <145649675+so2311@users.noreply.github.com>
2023-11-23 11:26:40 +0100

    Add files via upload

---
+++ blogeintragerfassen.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mein Blog</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        header {
+            text-align: center;
+            padding: 10px;
+            background-color: #333;
+            color: #fff;
+        }
+
+        main {
+            display: flex;
+            flex-wrap: wrap;
+        }
+
+        article {
+            flex: 1;
+            margin: 10px;
+            padding: 10px;
+            border: 1px solid #ddd;
+            background-color: #fff;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Mein Blog</h1>
+    </header>
+
+    <main>
+        <article>
+            <h2>Beitrag 1</h2>
+            <p>Dies ist der Text für den Blog-Beitrag.</p>
+        </article>
+		
+    </main>
+</body>
+</html>
